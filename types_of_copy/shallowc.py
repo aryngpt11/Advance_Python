ea1079c8752eb6adf517d8bc87d2b4cc78a656e4
VishalKumar1111 <aryan11gs@gmail.com>
2024-07-01 11:01:35 +0530

    shallow copy

--- types_of_copy/shallowc.py
+++ types_of_copy/shallowc.py
@@ -1,3 +1,25 @@
-a=['1','2','3']
+#normal copy using assignment operator
+
+""" a=['1','2','3']
 print(id(a))
 print(a)
+b=a
+print(id(b))
+print(b) """
+
+import copy
+a=[[1, 2], [3, 4]]
+b=copy.copy(a)
+print(id(a))
+print(a)
+
+print(id(b))
+print(b)
+
+b[1][0]=5
+
+print(id(a))
+print(a)
+
+print(id(b))
+print(b)
