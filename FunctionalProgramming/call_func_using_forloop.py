3ade1bbf635553e218e87990c3b6bbf8080cdf3a
VishalKumar1111 <aryan11gs@gmail.com>
2024-07-14 14:43:51 +0530

    Function

---
+++ FunctionalProgramming/call_func_using_forloop.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    print("Welcome")
+    print(f'add of {a} and {b} is {a+b}')
+
+for i in range(100):
+    c=int(input("Enter value of a:"))
+    d=int(input("Enter the value of b:"))
+    add(c,d)
---
+++ FunctionalProgramming/smpl_interest.py
@@ -0,0 +1,7 @@
+def SI(p,n,r):
+    print("Principle amt is: ",p)
+    print("number of years: ",n)
+    print("rate of interest: ",r)
+    si=(p*n*r)/100
+    print("Simple Interest is: ",si)
+SI(15000,3,5)
