0cffd598127b9cc42bdb2d1e104c57c508da6131
VishalKumar1111 <aryan11gs@gmail.com>
2024-07-15 14:33:18 +0530

    nested_lambda

--- FunctionalProgramming/nested_lambda_func.py
+++ FunctionalProgramming/nested_lambda_func.py
@@ -1,5 +1,20 @@
 #nested lambda function
 
-add=lambda x:lambda y:x+y
-func=add(10)
-print(func(30))
+# add=lambda x:lambda y:x+y
+# func=add(10)
+# print(func(30))
+
+# square=lambda n:n**2
+
+# def modify(f):
+#     num=int(input("Enter: "))
+#     return f(num)+num
+# print(modify(square))
+
+
+square=lambda n:n**2
+modify=lambda func:lambda num:func(num)+num
+# var=(modify(square))
+# print(var(int(input("enter the value: "))))
+#or
+print((modify(square)(4)))
